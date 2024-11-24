a419a1da16fa8f3cee70f442ff8324baf877e431
Benjamin Condori Vasquez <benjamincondori7@gmail.com>
2024-11-24 09:41:14 -0400

    solucionando bugs

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,24 +1,8 @@
-// (function() {
-
-//     const openButton = document.querySelector('.nav-menu');
-//     const menu = document.querySelector('.nav-link');
-//     const closeMenu = document.querySelector('.nav-close');
-
-//     openButton.addEventListener('click', () => {
-//         menu.classList.add('nav-link--show');
-//     });
-
-//     closeMenu.addEventListener('click', () => {
-//         menu.classList.remove('nav-link--show');
-//     });
-
-// })();
-
-
 function initNavigationMenu() {
     const openButton = document.querySelector('.nav-menu');
     const menu = document.querySelector('.nav-link');
     const closeMenu = document.querySelector('.nav-close');
+    const navLink = document.querySelectorAll('.nav-items a');
 
     if (openButton && menu && closeMenu) {
         openButton.addEventListener('click', () => {
@@ -27,6 +11,12 @@ function initNavigationMenu() {
 
         closeMenu.addEventListener('click', () => {
             menu.classList.remove('nav-link--show');
+        });
+        
+        navLink.forEach((link) => {
+            link.addEventListener('click', () => {
+                menu.classList.remove('nav-link--show');
+            });
         });
     }
     
--- public/assets/js/questions.js
+++ public/assets/js/questions.js
@@ -1,29 +1,3 @@
-// (function() {
-
-//     const titleQuestions = [...document.querySelectorAll('.questions-title')];
-
-//     console.log(titleQuestions);
-
-//     titleQuestions.forEach(question => {
-//         question.addEventListener('click', () => {
-//             let height = 0;
-//             let answer = question.nextElementSibling;
-//             let addPadding = question.parentElement.parentElement;
-
-//             addPadding.classList.toggle('questions-padding--add');
-//             question.children[0].classList.toggle('questions-arrow--rotate');
-
-//             if (answer.clientHeight === 0) {
-//                 height = answer.scrollHeight;
-//             }
-
-//             answer.style.height = `${height}px`;
-//         });
-//     });
-
-// })();
-
-
 function initQuestionsToggle() {
     const titleQuestions = [...document.querySelectorAll('.questions-title')];
 
